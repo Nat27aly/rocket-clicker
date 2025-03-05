1b432e35cc1a59fa3b6699d94afd4d343d1f66c2
Nat27aly <nat27aly@gmail.com>
2025-03-05 22:47:45 +0100

    Implemented basic env var config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,18 @@
 import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
+import {ENV} from "../config/env.js";
 
 const firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "..."
+    apiKey: ENV.FIREBASE.API_KEY,
+    authDomain: ENV.FIREBASE.AUTH_DOMAIN,
+    projectId: ENV.FIREBASE.PROJECT_ID,
+    storageBucket: ENV.FIREBASE.STORAGE_BUCKET,
+    messagingSenderId: ENV.FIREBASE.MESSAGING_SENDER_ID,
+    appId: ENV.FIREBASE.APP_ID
 };
+
+
 
 // Inicializa la app de Firebase
 const app = initializeApp(firebaseConfig);
