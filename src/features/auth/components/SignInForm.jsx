68575873b3335843e3ef52f6b161a751ab23077d
TheBos-s <mkpako2009@gmail.com>
2025-03-06 07:59:11 +0100

    Validation for user registration and login

--- src/features/auth/components/SignInForm.jsx
+++ src/features/auth/components/SignInForm.jsx
@@ -1,6 +1,8 @@
 import Input from "../../../components/Input.jsx";
 import AuthSubmitButton from "./AuthSubmitButton.jsx";
-import {useState} from "react";
+import { useState } from "react";
+import { useAuth } from "../context/authContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 function SignInForm() {
     const [formData, setFormData] = useState({
@@ -8,12 +10,10 @@ function SignInForm() {
         password: ''
     })
 
-    async function handleSubmit(event) {
-        event.preventDefault();
 
-        // Aqui podemos enviar la info a firebase
-        console.log('Enviamos los datos:', formData);
-    }
+    const { signin } = useAuth();
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
 
     // Esta funcion se llama cada vez que el input detecta un cambio
     function handleChange(event) {
@@ -28,26 +28,65 @@ function SignInForm() {
         setFormData(newFormData);
     }
 
+    async function handleSubmit(event) {
+        event.preventDefault();
+        setError(''); // Limpiar el error previo
+
+        if (!formData.email || !formData.password) {
+            setError("Debes completar los campos email y contraseña");
+            return;
+        }
+
+        try {
+            await signin(formData.email, formData.password);
+            navigate('/testHome'); // Redirige al inicio o a la página principal
+        } catch (error) {
+            // Maneja el error de autenticación
+            switch (error.code) {
+                case 'auth/user-not-found':
+                    setError("El usuario no está registrado");
+                    break;
+                case 'auth/wrong-password':
+                    setError("La contraseña es incorrecta");
+                    break;
+                case 'auth/invalid-email':
+                    setError("El correo electrónico no tiene un formato válido");
+                    break;
+                default:
+                    setError("Hubo un error en el inicio de sesión");
+                    break;
+            }
+        }
+    }
+
+
+
     return (
-        <form className="w-full space-y-3" onSubmit={handleSubmit}>
-            <Input
-                type="text"
-                name="email"
-                label="Email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="nombre@mail.com"
-            />
-            <Input
-                type="password"
-                name="password"
-                label="Contraseña"
-                value={formData.password}
-                onChange={handleChange}
-                placeholder=""
-            />
-            <AuthSubmitButton>Ingresar</AuthSubmitButton>
-        </form>
+        <div>
+            <form className="w-full space-y-3" onSubmit={handleSubmit}>
+                <Input
+                    type="text"
+                    name="email"
+                    id="email"
+                    label="Email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="nombre@mail.com"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    id="password"
+                    label="Contraseña"
+                    value={formData.password}
+                    onChange={handleChange}
+                    placeholder=""
+                />
+                <AuthSubmitButton>Ingresar</AuthSubmitButton>
+            </form>
+
+            {error && <p>{error}</p>}
+        </div>
     );
 }
 
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,6 +2,7 @@ import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
+/*
 const firebaseConfig = {
     apiKey: "...",
     authDomain: "...",
@@ -9,7 +10,17 @@ const firebaseConfig = {
     storageBucket: "...",
     messagingSenderId: "...",
     appId: "..."
-};
+};*/
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDj2tyTXd5izQywW5qiSQVSv0q0W2Gw9Uk",
+    authDomain: "rockclicker-linkia.firebaseapp.com",
+    projectId: "rockclicker-linkia",
+    storageBucket: "rockclicker-linkia.firebasestorage.app",
+    messagingSenderId: "709765897570",
+    appId: "1:709765897570:web:c37c1646fec5ac4251603b",
+    measurementId: "G-GG6KW208Y2"
+  };
 
 // Inicializa la app de Firebase
 const app = initializeApp(firebaseConfig);
